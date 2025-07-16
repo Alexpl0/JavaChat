b123b1b723a6ac8e3c1669ca1084de6e26b81855
Yusmany <yusmany.sandoval@e-openroad.com>
2025-07-15 22:10:37 -0600

    Se incorpora JTextArea y JTextField a la ventana

--- client/ChatClientGUI.java
+++ client/ChatClientGUI.java
@@ -1,13 +1,29 @@
-import javax.swing.*;
+import java.awt.*;
+import javax.swing.*;  
 
 public class ChatClientGUI {
 
     private JFrame frame;
+    private JTextArea chatArea;
+    private JTextField inputField;
 
     public ChatClientGUI() {
+   
         frame = new JFrame("Cliente de Chat");
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());  
+
+    
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+
+        inputField = new JTextField();
+
+        
+        frame.add(chatArea, BorderLayout.CENTER);
+        frame.add(inputField, BorderLayout.SOUTH);
+
         frame.setVisible(true);
     }
 
