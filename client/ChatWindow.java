d6f6ad77c3a99cddd3045a6648ff0adf5427720e
Yusmany <yusmany.sandoval@e-openroad.com>
2025-07-15 22:25:26 -0600

    Extrae ChatWindow como componente reutilizable

---
+++ client/ChatWindow.java
@@ -0,0 +1,32 @@
+package com.client;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ChatWindow extends JPanel {
+    private JTextArea chatArea;
+    private JTextField inputField;
+    private JButton sendButton;
+
+    public ChatWindow() {
+        setLayout(new BorderLayout());
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(chatArea);
+
+        inputField = new JTextField();
+        sendButton = new JButton("Enviar");
+
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputPanel.add(inputField, BorderLayout.CENTER);
+        inputPanel.add(sendButton, BorderLayout.EAST);
+
+        add(scrollPane, BorderLayout.CENTER);
+        add(inputPanel, BorderLayout.SOUTH);
+    }
+
+    public JTextArea getChatArea() { return chatArea; }
+    public JTextField getInputField() { return inputField; }
+    public JButton getSendButton() { return sendButton; }
+}
